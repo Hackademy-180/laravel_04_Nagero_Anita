57264b25bfd2a69758468347aa4a79681737e9bb
anita <nageroa6@gmail.com>
2026-06-13 01:53:59 +0200

    Primo commit inizio del lavoro

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+    <!-- SCRIPT FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- LINK BTS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- estate -->
+    <i class="fa-thin fa-flower-tulip" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-regular fa-flower-tulip" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-thin fa-umbrella-beach" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-solid fa-flower-daffodil" style="color: rgb(105, 235, 235);"></i>
+    <!-- PRIMAVERA -->
+    <i class="fa-thin fa-flower-daffodil" style="color: rgb(105, 235, 235);"></i>
+    <!-- INVERNO -->
+    <i class="fa-thin fa-snowflake" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-regular fa-snow-blowing" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-thin fa-snow-blowing" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-solid fa-snowflake" style="color: rgb(105, 235, 235);"></i>
+    <!-- autunno -->
+    <i class="fa-solid fa-hand-shaka" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-light fa-hand-shaka" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-thin fa-wind" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-solid fa-wind" style="color: rgb(105, 235, 235);"></i>
+
+    <!-- SCRIPT IKONS -->
+    <i class="fa-solid fa-umbrella-beach" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-light fa-watermelon-slice" style="color: rgb(105, 235, 235);"></i>
+    <i class="fa-solid fa-watermelon-slice" style="color: rgb(105, 235, 235);"></i>
+
+    <script src="https://kit.fontawesome.com/daf6ff33d9.js" crossorigin="anonymous"></script>
+
+
+    <!-- SCRIPT BTS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
